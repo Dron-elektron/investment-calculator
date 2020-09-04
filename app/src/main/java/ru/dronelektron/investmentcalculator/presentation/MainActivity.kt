c5063293bb6cc2da8ac34f75969b083a256780f7
Dron-elektron <dron.elektron@gmail.com>
2020-09-04 13:24:11 +0300

    Create presentation layer

--- app/src/main/java/ru/dronelektron/investmentcalculator/presentation/MainActivity.kt
+++ app/src/main/java/ru/dronelektron/investmentcalculator/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package ru.dronelektron.investmentcalculator
+package ru.dronelektron.investmentcalculator.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.dronelektron.investmentcalculator.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
