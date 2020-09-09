d5a2d5294bd095097d56733204775aa8aa5dac5a
Dron-elektron <dron.elektron@gmail.com>
2020-09-09 11:31:34 +0300

    Force to use dots instead of commas in "balance" and "percent" textviews

--- app/src/main/java/ru/dronelektron/investmentcalculator/presentation/investresult/BindingExt.kt
+++ app/src/main/java/ru/dronelektron/investmentcalculator/presentation/investresult/BindingExt.kt
@@ -1,10 +1,16 @@
 package ru.dronelektron.investmentcalculator.presentation.investresult
 
 import android.view.View
+import android.widget.TextView
+import androidx.annotation.StringRes
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import ru.dronelektron.investmentcalculator.R
 import ru.dronelektron.investmentcalculator.domain.model.Profit
 import ru.dronelektron.investmentcalculator.presentation.investresult.profitslist.ProfitsAdapter
+import java.util.*
+
+private val DEFAULT_LOCALE = Locale.US
 
 @BindingAdapter("profits")
 fun setProfits(recycler: RecyclerView, profits: List<Profit>?) {
@@ -14,4 +20,20 @@ fun setProfits(recycler: RecyclerView, profits: List<Profit>?) {
 @BindingAdapter("visibleIf")
 fun setVisibleIf(view: View, isVisible: Boolean) {
     view.visibility = if (isVisible) View.VISIBLE else View.GONE
+}
+
+@BindingAdapter("balanceFormat")
+fun balanceFormat(view: TextView, balance: Double) {
+    decimalFormat(view, R.string.profit_balance_format, balance)
+}
+
+@BindingAdapter("percentFormat")
+fun percentFormat(view: TextView, percent: Double) {
+    decimalFormat(view, R.string.profit_percent_format, percent)
+}
+
+private fun decimalFormat(view: TextView, @StringRes formatRes: Int, value: Double) {
+    val textFormat = view.resources.getString(formatRes)
+
+    view.text = String.format(DEFAULT_LOCALE, textFormat, value)
 }
