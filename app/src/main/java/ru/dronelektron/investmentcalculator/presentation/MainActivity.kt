9ee12d4ade8f4560aaeef14a3231a1482b52898b
Dron-elektron <dron.elektron@gmail.com>
2020-09-18 14:29:04 +0300

    Add screen with chart for profits
    Add menu item "Show chart" in investment result screen.
    Add MPAndroidChart dependency.
    Setup toolbar as action bar.

--- app/src/main/java/ru/dronelektron/investmentcalculator/presentation/MainActivity.kt
+++ app/src/main/java/ru/dronelektron/investmentcalculator/presentation/MainActivity.kt
@@ -4,18 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import ru.dronelektron.investmentcalculator.R
 import ru.dronelektron.investmentcalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val navController by lazy {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        navHostFragment.navController
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        val toolbar = binding.toolbar
+        val appBarConfig = AppBarConfiguration(setOf(R.id.invest_form_fragment))
 
-        binding.toolbar.setupWithNavController(navController)
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController, appBarConfig)
     }
+
+    override fun onSupportNavigateUp() = navController.navigateUp() || super.onSupportNavigateUp()
 }
--- app/src/main/java/ru/dronelektron/investmentcalculator/presentation/investresult/InvestResultFragment.kt
+++ app/src/main/java/ru/dronelektron/investmentcalculator/presentation/investresult/InvestResultFragment.kt
@@ -2,18 +2,29 @@ package ru.dronelektron.investmentcalculator.presentation.investresult
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import ru.dronelektron.investmentcalculator.R
 import ru.dronelektron.investmentcalculator.databinding.FragmentInvestResultBinding
 import ru.dronelektron.investmentcalculator.presentation.investform.InvestFormViewModel
 import ru.dronelektron.investmentcalculator.presentation.investresult.profitslist.ProfitsAdapter
 
 class InvestResultFragment : Fragment() {
     private val investFormViewModel by activityViewModels<InvestFormViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding = FragmentInvestResultBinding.inflate(inflater, container, false)
@@ -23,6 +34,20 @@ class InvestResultFragment : Fragment() {
         setupProfits(binding.profits)
 
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_invest_result, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_chart) {
+            findNavController().navigate(R.id.action_invest_result_fragment_to_invest_result_chart_fragment)
+
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
     private fun setupProfits(recycler: RecyclerView) {
